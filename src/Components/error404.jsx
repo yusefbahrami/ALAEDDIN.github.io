85be105e1597ffd0730663a0c0e0fc4d6253171f
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-12-16 02:23:37 +0330

    Error404 component was completed

--- src/Components/error404.jsx
+++ src/Components/error404.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const Error404 = () => {
   return (
     <div className="error-countainer">
-      <h2>ERROR 404</h2>
+      <span>?</span>
+      <div className="error-desciption-countainer">
+        <p className="error-title">Somthing's wrong here...</p>
+        <p className="error-description">
+          We can't find the page you're looking for.
+        </p>
+      </div>
     </div>
   );
 };
