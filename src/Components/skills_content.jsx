f98d4d0db4f8d01f73867f612294a2cb2bb1be5b
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-12-16 20:21:21 +0330

    some class names were added

--- src/Components/skills_content.jsx
+++ src/Components/skills_content.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const SkillsContent = () => {
   return (
     <div className="skills-content" id="skills-content">
-      <div className="skill-description">
+      <div className="skill-description javascript">
         <Link href="#" className="skill-route">
           <div className="skill-description-title">
             {/* <i class="uil uil-java-script icon"></i> */}
@@ -22,11 +22,12 @@ const SkillsContent = () => {
           </div>
         </Link>
       </div>
-      <div className="skill-description">
+      <div className="skill-description typescript">
         <Link href="#" className="skill-route">
           <div className="skill-description-title">
-            <i class="devicon-python-plain  icon"></i>
-            <h3 className="skill-description-inner-title">Python</h3>
+            {/* <i class="uil uil-java-script icon"></i> */}
+            <i class="devicon-typescript-plain icon"></i>
+            <h3 className="skill-description-inner-title">Typescript</h3>
           </div>
           <div className="skill-description-text">
             <p className="skill-description-inner-text">
@@ -40,7 +41,7 @@ const SkillsContent = () => {
           </div>
         </Link>
       </div>
-      <div className="skill-description">
+      <div className="skill-description react">
         <Link href="#" className="skill-route">
           <div className="skill-description-title">
             {/* <i className="uil uil-heart icon"></i> */}
@@ -59,7 +60,26 @@ const SkillsContent = () => {
           </div>
         </Link>
       </div>
-      <div className="skill-description">
+      <div className="skill-description python">
+        <Link href="#" className="skill-route">
+          <div className="skill-description-title">
+            <i class="devicon-python-plain  icon"></i>
+            <h3 className="skill-description-inner-title">Python</h3>
+          </div>
+          <div className="skill-description-text">
+            <p className="skill-description-inner-text">
+              Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Corporis illum cumque, dolorum
+              voluptatum voluptatem culpa inventore, minima exercitationem
+              provident quo error in laborum necessitatibus aliquam eos
+              voluptatibus, eius aliquid obcaecati!adipisicing elit. Impedit,
+              eligendi!
+            </p>
+          </div>
+        </Link>
+      </div>
+
+      <div className="skill-description photoshop">
         <Link href="#" className="skill-route">
           <div className="skill-description-title">
             <i class="devicon-photoshop-line icon"></i>
