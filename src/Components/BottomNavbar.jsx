49027cd3d2db7f6032249b7add711f110e93650f
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-12-21 17:22:40 +0330

    added some test code of swal2

--- src/Components/BottomNavbar.jsx
+++ src/Components/BottomNavbar.jsx
@@ -4,6 +4,7 @@ import { ThemeContext } from "../Context/themeContext";
 import LinkToUser from "./bottom_navbar_sub_components/LinkToUserPanel";
 import Home from "./bottom_navbar_sub_components/ReturnToHome";
 import ScrollToTop from "./bottom_navbar_sub_components/ScrollToTop";
+import Swal from "sweetalert2";
 
 const BottomNavbar = (props, ref) => {
   const { islight, setIsLight } = useContext(ThemeContext);
@@ -19,16 +20,45 @@ const BottomNavbar = (props, ref) => {
     }
   };
 
-  useEffect(() => {
-    handleSeitchTheme();
-  }, []);
+  // test swal2
+  // const handleShowMessage = () => {
+  //   Swal.fire({
+  //     title: "test",
+  //     titleText: "title text",
+  //     text: "I will close in 2 seconds.",
+  //     timer: 2000,
+  //     icon: "success",
+  //     color: "#da0037",
+  //     background: "#171717",
+  //     footer: "footer",
+  //     confirmButtonColor: "#da0037",
+  //   });
+  // };
+
+  const handleShowToast = () => {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "bottom-end",
+      timer: 1500,
+      timerProgressBar: true,
+      showConfirmButton: false,
+      background: `${islight ? "#171717" : "effffd"}`,
+    });
+    Toast.fire({
+      icon: "success",
+      title: "title for this",
+      text: "this is the test text",
+      color: `${islight ? "#da0037" : "#42c2ff"}`,
+    });
+  };
+  // End test
 
   return (
     <div className="nav-countainer">
       <nav className="nav-bar">
         <ul>
           <LinkToUser />
-          <li className="nav-item">
+          <li className="nav-item" onClick={handleShowToast}>
             <Link to="#">
               <i className="uil uil-setting"></i>
               <span>SETTING</span>
