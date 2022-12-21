ba2bd1d29d23738b97e9c768368a2daabd4a9b8a
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-12-21 21:53:04 +0330

    Better pagination

--- src/Components/ProgrammingSkillsContent.jsx
+++ src/Components/ProgrammingSkillsContent.jsx
@@ -3,114 +3,117 @@ import { Link } from "react-router-dom";
 const SkillsContent = () => {
   return (
     <div className="skills-content" id="skills-content">
-      <div className="skill-description javascript">
-        <Link to="#" className="skill-route">
-          <div className="skill-description-title">
-            <i className="devicon-nodejs-plain icon"></i>
-            <h3 className="skill-description-inner-title">Javascript</h3>
-          </div>
-          <div className="skill-description-text">
-            <p className="skill-description-inner-text">
-              Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Corporis illum cumque, dolorum
-              voluptatum voluptatem culpa inventore, minima exercitationem
-              provident quo error in laborum necessitatibus aliquam eos
-              voluptatibus, eius aliquid obcaecati!adipisicing elit. Impedit,
-              eligendi!
-            </p>
-          </div>
-        </Link>
-      </div>
-      <div className="skill-description react">
-        <Link to="#" className="skill-route">
-          <div className="skill-description-title">
-            <i className="devicon-react-original icon"></i>
-            <h3 className="skill-description-inner-title">React js</h3>
-          </div>
-          <div className="skill-description-text">
-            <p className="skill-description-inner-text">
-              Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Corporis illum cumque, dolorum
-              voluptatum voluptatem culpa inventore, minima exercitationem
-              provident quo error in laborum necessitatibus aliquam eos
-              voluptatibus, eius aliquid obcaecati!adipisicing elit. Impedit,
-              eligendi!
-            </p>
-          </div>
-        </Link>
-      </div>
-      <div className="skill-description typescript">
-        <Link to="#" className="skill-route">
-          <div className="skill-description-title">
-            <i className="devicon-typescript-plain icon"></i>
-            <h3 className="skill-description-inner-title">Typescript</h3>
-          </div>
-          <div className="skill-description-text">
-            <p className="skill-description-inner-text">
-              Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Corporis illum cumque, dolorum
-              voluptatum voluptatem culpa inventore, minima exercitationem
-              provident quo error in laborum necessitatibus aliquam eos
-              voluptatibus, eius aliquid obcaecati!adipisicing elit. Impedit,
-              eligendi!
-            </p>
-          </div>
-        </Link>
-      </div>
-      <div className="skill-description Solidity">
-        <Link to="#" className="skill-route">
-          <div className="skill-description-title">
-            <i className="devicon-typescript-plain icon"></i>
-            <h3 className="skill-description-inner-title">Solidity</h3>
-          </div>
-          <div className="skill-description-text">
-            <p className="skill-description-inner-text">
-              Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Corporis illum cumque, dolorum
-              voluptatum voluptatem culpa inventore, minima exercitationem
-              provident quo error in laborum necessitatibus aliquam eos
-              voluptatibus, eius aliquid obcaecati!adipisicing elit. Impedit,
-              eligendi!
-            </p>
-          </div>
-        </Link>
-      </div>
-      <div className="skill-description python">
-        <Link to="#" className="skill-route">
-          <div className="skill-description-title">
-            <i className="devicon-python-plain  icon"></i>
-            <h3 className="skill-description-inner-title">Python</h3>
-          </div>
-          <div className="skill-description-text">
-            <p className="skill-description-inner-text">
-              Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Corporis illum cumque, dolorum
-              voluptatum voluptatem culpa inventore, minima exercitationem
-              provident quo error in laborum necessitatibus aliquam eos
-              voluptatibus, eius aliquid obcaecati!adipisicing elit. Impedit,
-              eligendi!
-            </p>
-          </div>
-        </Link>
-      </div>
+      <h2 className="programming-skill-title">Programming</h2>
+      <div className="programming-skill-content">
+        <div className="skill-description javascript">
+          <Link to="#" className="skill-route">
+            <div className="skill-description-title">
+              <i className="devicon-nodejs-plain icon"></i>
+              <h3 className="skill-description-inner-title">Javascript</h3>
+            </div>
+            <div className="skill-description-text">
+              <p className="skill-description-inner-text">
+                Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. Corporis illum cumque,
+                dolorum voluptatum voluptatem culpa inventore, minima
+                exercitationem provident quo error in laborum necessitatibus
+                aliquam eos voluptatibus, eius aliquid obcaecati!adipisicing
+                elit. Impedit, eligendi!
+              </p>
+            </div>
+          </Link>
+        </div>
+        <div className="skill-description react">
+          <Link to="#" className="skill-route">
+            <div className="skill-description-title">
+              <i className="devicon-react-original icon"></i>
+              <h3 className="skill-description-inner-title">React js</h3>
+            </div>
+            <div className="skill-description-text">
+              <p className="skill-description-inner-text">
+                Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. Corporis illum cumque,
+                dolorum voluptatum voluptatem culpa inventore, minima
+                exercitationem provident quo error in laborum necessitatibus
+                aliquam eos voluptatibus, eius aliquid obcaecati!adipisicing
+                elit. Impedit, eligendi!
+              </p>
+            </div>
+          </Link>
+        </div>
+        <div className="skill-description typescript">
+          <Link to="#" className="skill-route">
+            <div className="skill-description-title">
+              <i className="devicon-typescript-plain icon"></i>
+              <h3 className="skill-description-inner-title">Typescript</h3>
+            </div>
+            <div className="skill-description-text">
+              <p className="skill-description-inner-text">
+                Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. Corporis illum cumque,
+                dolorum voluptatum voluptatem culpa inventore, minima
+                exercitationem provident quo error in laborum necessitatibus
+                aliquam eos voluptatibus, eius aliquid obcaecati!adipisicing
+                elit. Impedit, eligendi!
+              </p>
+            </div>
+          </Link>
+        </div>
+        <div className="skill-description Solidity">
+          <Link to="#" className="skill-route">
+            <div className="skill-description-title">
+              <i className="devicon-typescript-plain icon"></i>
+              <h3 className="skill-description-inner-title">Solidity</h3>
+            </div>
+            <div className="skill-description-text">
+              <p className="skill-description-inner-text">
+                Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. Corporis illum cumque,
+                dolorum voluptatum voluptatem culpa inventore, minima
+                exercitationem provident quo error in laborum necessitatibus
+                aliquam eos voluptatibus, eius aliquid obcaecati!adipisicing
+                elit. Impedit, eligendi!
+              </p>
+            </div>
+          </Link>
+        </div>
+        <div className="skill-description python">
+          <Link to="#" className="skill-route">
+            <div className="skill-description-title">
+              <i className="devicon-python-plain  icon"></i>
+              <h3 className="skill-description-inner-title">Python</h3>
+            </div>
+            <div className="skill-description-text">
+              <p className="skill-description-inner-text">
+                Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. Corporis illum cumque,
+                dolorum voluptatum voluptatem culpa inventore, minima
+                exercitationem provident quo error in laborum necessitatibus
+                aliquam eos voluptatibus, eius aliquid obcaecati!adipisicing
+                elit. Impedit, eligendi!
+              </p>
+            </div>
+          </Link>
+        </div>
 
-      <div className="skill-description photoshop">
-        <Link to="#" className="skill-route">
-          <div className="skill-description-title">
-            <i className="devicon-photoshop-line icon"></i>
-            <h3 className="skill-description-inner-title">Photoshop</h3>
-          </div>
-          <div className="skill-description-text">
-            <p className="skill-description-inner-text">
-              Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Corporis illum cumque, dolorum
-              voluptatum voluptatem culpa inventore, minima exercitationem
-              provident quo error in laborum necessitatibus aliquam eos
-              voluptatibus, eius aliquid obcaecati!adipisicing elit. Impedit,
-              eligendi!
-            </p>
-          </div>
-        </Link>
+        <div className="skill-description photoshop">
+          <Link to="#" className="skill-route">
+            <div className="skill-description-title">
+              <i className="devicon-photoshop-line icon"></i>
+              <h3 className="skill-description-inner-title">Photoshop</h3>
+            </div>
+            <div className="skill-description-text">
+              <p className="skill-description-inner-text">
+                Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. Corporis illum cumque,
+                dolorum voluptatum voluptatem culpa inventore, minima
+                exercitationem provident quo error in laborum necessitatibus
+                aliquam eos voluptatibus, eius aliquid obcaecati!adipisicing
+                elit. Impedit, eligendi!
+              </p>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
