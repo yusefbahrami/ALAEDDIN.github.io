cb56c60ee55b272a96d97732da5248ea452530a0
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2023-01-17 21:32:42 +0330

    user login route was added

--- src/Components/bottom_navbar_sub_components/LinkToUserPanel.jsx
+++ src/Components/bottom_navbar_sub_components/LinkToUserPanel.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import userProfile from "../../assets/DarkProfile.jpg";
+import { UserTokenIdContext } from "../../Context/UserTokenIdContext";
 
 const LinkToUser = () => {
+  // const UserTokenId=localStorage.getItem("UserTokenId")
+  const UserTokenId = useContext(UserTokenIdContext);
+
   return (
     <li className="nav-item">
-      <Link to="#">
+      <Link
+        to={
+          UserTokenId.UserTokenId
+            ? `/user/${UserTokenId.UserTokenId}`
+            : "/login"
+        }
+      >
         {/* <i className="uil uil-user"></i> */}
         <img className="user-profile" src={userProfile} alt="Profile" />
         <span>USER</span>
