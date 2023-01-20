607432558835b42be41d1ad068bd012c949b6c91
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2023-01-20 15:57:21 +0330

    sub components was added

--- src/Components/Login_Signin/LoginPanel.jsx
+++ src/Components/Login_Signin/LoginPanel.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import EmailInput from "./SubComponents/EmailInput";
+import PasswordInput from "./SubComponents/PasswordInput";
+import FormTitle from "./SubComponents/FormTitle";
+import { NavLink } from "react-router-dom";
+import SubmitButton from "./SubComponents/SubmitButton";
 
 const LoginPanel = () => {
-  return <h3>in login</h3>;
+  return (
+    <div className="login-countainer">
+      <FormTitle title={'Login'}/>
+      <form>
+        <EmailInput />
+        <PasswordInput/>
+        <SubmitButton text={'Login'}/>
+      </form>
+      <NavLink to='/signin' className='link-to-signin'>
+        Signin
+      </NavLink>
+    </div>
+  );
 };
 export default LoginPanel;
