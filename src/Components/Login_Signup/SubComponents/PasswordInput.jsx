b193e942acdc47dd485bd17bc20a93b20a38473b
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2023-01-22 22:41:12 +0330

    ref was added

--- src/Components/Login_Signup/SubComponents/PasswordInput.jsx
+++ src/Components/Login_Signup/SubComponents/PasswordInput.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { forwardRef } from "react";
 
-const PasswordInput=(props)=>{
-    return(<input type={'password'} placeholder={`${props.text}`} className='form-input password-input' required/>)
-}
-export default PasswordInput
+const PasswordInput = (props, ref) => {
+  return (
+    <input
+      ref={ref}
+      type={"password"}
+      placeholder={`${props.text}`}
+      className="form-input password-input"
+      required
+    />
+  );
+};
+export default forwardRef(PasswordInput);
