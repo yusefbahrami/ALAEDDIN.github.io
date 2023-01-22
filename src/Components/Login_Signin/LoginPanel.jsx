c15bc4349d1efc0aa4674a1a3965bd06e0aef078
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2023-01-22 11:44:50 +0330

    completed and waiting for api

--- src/Components/Login_Signin/LoginPanel.jsx
+++ src/Components/Login_Signin/LoginPanel.jsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { createRef, useState } from "react";
 import EmailInput from "./SubComponents/EmailInput";
 import PasswordInput from "./SubComponents/PasswordInput";
 import FormTitle from "./SubComponents/FormTitle";
 import { NavLink } from "react-router-dom";
 import SubmitButton from "./SubComponents/SubmitButton";
+import validator from "validator";
 
 const LoginPanel = () => {
+  const EmailInputRef = createRef();
+  const [emailError, setEmailError] = useState("");
+  const validateEmail = (e) => {
+    e.preventDefault();
+    var email = EmailInputRef.current.value;
+    console.log(EmailInputRef.current.value);
+    if (validator.isEmail(email)) {
+      setEmailError("");
+    } else {
+      setEmailError("The Email Invalid!");
+    }
+  };
   return (
     <div className="login-countainer">
-      <FormTitle title={'Login'}/>
-      <form>
-        <EmailInput />
-        <PasswordInput/>
-        <SubmitButton text={'Login'}/>
+      <FormTitle title={"Login"} />
+      <form onSubmit={validateEmail}>
+        <EmailInput ref={EmailInputRef} />
+        <span className="email-error-label">{emailError}</span>
+        <PasswordInput />
+        <SubmitButton text={"Login"} />
       </form>
-      <NavLink to='/signin' className='link-to-signin'>
-        Signin
-      </NavLink>
+      <span className="link-to-signin">
+        Not registered yet?
+        <NavLink to="/signin"> Create an Account</NavLink>
+      </span>
     </div>
   );
 };
