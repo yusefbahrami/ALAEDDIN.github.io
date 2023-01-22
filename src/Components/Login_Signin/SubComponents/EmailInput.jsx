f5c06e557bde0d69af2c6d8952aa9d994d6062c5
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2023-01-22 11:45:28 +0330

    completed

--- src/Components/Login_Signin/SubComponents/EmailInput.jsx
+++ src/Components/Login_Signin/SubComponents/EmailInput.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
-
-const EmailInput=()=>{
-    return(<input type={'email'} placeholder='example@email.com' className='form-input Email-input'/>)
-}
-export default EmailInput
+import React, { forwardRef } from "react";
+const EmailInput = (props, ref) => {
+  return (
+    <input
+      type={"email"}
+      placeholder="example@email.com"
+      className="form-input Email-input"
+      ref={ref}
+      required
+    />
+  );
+};
+export default forwardRef(EmailInput);
